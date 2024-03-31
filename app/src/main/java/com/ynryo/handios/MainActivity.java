6ecfae90e176610ae78b821f2825ebaa8153fcd6
Ynryo <ewenatideklein@gmail.com>
2024-03-31 19:18:39 +0200

    commit

---
+++ app/src/main/java/com/ynryo/handios/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ynryo.handios;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView handios;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        handios = findViewById(R.id.handios);
+        handios.getSettings().setJavaScriptEnabled(true);
+        handios.setWebViewClient(new WebViewClient());
+        handios.loadUrl("http://handios.specstech.fr/");
+    }
+}
